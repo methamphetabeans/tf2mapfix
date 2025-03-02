15f8a7dba24cf43e5ac9a69297edde47fde714af
methamphetabeans <iamzac67@gmail.com>
2025-03-02 05:15:28 -0600

    Added help message, added ability to fix individual map files. Cleaned up code, supposedly.

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # tf2mapfix
-A shell script to fix broken maps on TF2 Linux
+A shell script to fix broken community maps on TF2 Linux
+If a community map is missing a bunch of textures or other resources, this script will *probably* fix it.
 
-This script is intended to fix custom maps that have broken textures (and other missing resources)
-
-This script will take every map from your TF2 downloads, decompile it with VPKEdit, and put the unpacked files into your TF2 custom directory for you.
-
-By default, every map will be processed. I (might) add a feature later that allows you to specify individual maps to process.
+This script will take map files from your TF2 downloads, decompile them with VPKEdit, and put the unpacked files into your TF2 custom directory for you.
 
 # Installation
 First download the Linux VPKEdit CLI from [here](https://github.com/craftablescience/VPKEdit/releases/download/v4.4.1/VPKEdit-Linux-Standalone-CLI-gcc-Release.zip)
@@ -14,13 +11,21 @@ Extract the zip to whatever destination you want to have it.
 Next, download tf2mapfix.sh and put it the VPKEditCLI directory you created.
 
 # Running the script
-Make sure tf2mapfix.sh has execute permissions and run the sh file in the terminal from your VPKEdit directory:
+Make sure tf2mapfix.sh has execute permissions and run the sh file in the terminal from your VPKEdit directory.
+
+To run this script on all map files:
 ```
 ./tf2mapfix.sh
+```
+
+To run this script on specific map files:
+```
+./tf2mapfix.sh -f
+./tf2mapfix.sh --files      #the script will prompt you for what files you want to use
 ```
 
 The script will first look for your TF2 directory in its default location, and if it is not found, will prompt the user to input their installation directory. This location will be saved in a config file for later use.
 
 # Possible Issues
 
-There will likely be some sort of bug from a use case I haven't be able to test. Feel free to contact me @ thebeandev@gmail.com or try to fix these yourself.
+There will likely be some sort of bug from a use case I haven't be able to test. Feel free to contact me @ thebeandev@gmail.com, or try to fix these yourself.
--- tf2mapfix.sh
+++ tf2mapfix.sh
@@ -4,64 +4,121 @@
 
 GREEN='\033[0;32m'
 NC='\033[0m'
+TF2DIR="~/.steam/steam/steamapps/common"
+TF2MAPSDIR=""
+TF2CUSTOMDIR=""
 
-TF2DIR="~/.steam/steam/steamapps/common/Team Fortress 2/"
 
-function get_directory(){
-	if [ -d "$TF2DIR" ]; then
-		echo "$TF2DIR"
+show_help() {
+	echo "TF2MAPFIX"
+	echo ""
+	echo "This script extracts map files using VPKEdit and copies the decompiled files to the 'tf/custom' directory to fix broken resources in game"
+	echo "The script will check for the default location of TF2, however alternate installation locations will be saved to the config for use if necessary"
+	echo ""
+	echo "Usage: "
+	echo "	tf2mapfix [argument]"
+	echo ""
+	echo "Options: "
+	echo "	Run command with no arguments to extract all downloaded TF2 maps and copy files to 'tf/custom'"
+	echo "	-f, --files		Extract individual TF2 maps and copy files to 'tf/custom'"
+	echo "	-h, --help		Show this help message and exit"
+	echo ""
+	echo "For support, or to report a bug, email thebeandev@gmail.com"
+	exit 0
+}
+
+get_directory(){
+	#if statement hell.
+
+	# check if conf exists
+	if [ -f "./tf2mapfixconfig" ]; then
+		source tf2mapfixconfig
+
+		# if exists, check if conf is correct
+		# conf exists, is tf2 actually here?
+		if [ -f "$TF2WORKINGDIR/tf_linux64" ]; then
+			TF2WORKINGDIR=$(echo "$TF2WORKINGDIR" | sed 's/\\ / /g')
+			TF2MAPSDIR="$TF2WORKINGDIR/tf/download/maps"
+			TF2CUSTOMDIR="$TF2WORKINGDIR/tf/custom"
+
+		# tf2 is not where the config says it is, get directory from scratch
+		else
+			directory_fallback
+		fi
+	# conf does not exist, get directory from scratch
 	else
-		read -rep $'TF2 Directory not found at its default location. Please input your TF2 Directory location \n(This will usually end with \'.../steamapps/common/Team Fortress 2\' use the Tab key to autocomplete and avoid issues with spaces) \n' TF2ALTDIR
-		DIRESCAPED=$(echo "$TF2ALTDIR" | sed 's/\\ / /g')
-		echo "$DIRESCAPED"
+		directory_fallback
+	fi
+
+	# save directory location for future use
+	echo "TF2WORKINGDIR=\"$TF2WORKINGDIR\"" > tf2mapfixconfig
+}
+
+directory_fallback(){
+	if [ -f "$TF2DIR/tf_linux64" ]; then
+		# if yes, continue
+		TF2WORKINGDIR=$(echo "$TF2DIR" | sed 's/\\ / /g')
+		TF2MAPSDIR="$TF2WORKINGDIR/tf/download/maps"
+		TF2CUSTOMDIR="$TF2WORKINGDIR/tf/custom"
+	else
+		# if not, prompt for input until correct
+		while [ ! -f "$TF2WORKINGDIR/tf_linux64" ]
+		do
+			read -rep $'TF2 Directory not found. Please input your TF2 Directory location \n(This will usually end with \'.../steamapps/common/Team Fortress 2\' use the Tab key to autocomplete and avoid issues with spaces) \n' TF2ALTDIR
+			TF2WORKINGDIR=$(echo "$TF2ALTDIR" | sed 's/\\ / /g')
+			TF2MAPSDIR="$TF2WORKINGDIR/tf/download/maps"
+			TF2CUSTOMDIR="$TF2WORKINGDIR/tf/custom"
+		done
 	fi
 }
 
-#check if config exists
-if [ -f "./tf2mapfixconfig" ]; then
-	source tf2mapfixconfig
-fi
+select_files(){
+	MAPFIXDIR=$(pwd)
+	cd "$TF2MAPSDIR"
+	read -rep $'What maps would you like to fix? (Autocomplete enabled) \n' MAPSELECTION
+	cd "$MAPFIXDIR"
+	echo $MAPSELECTION
+}
 
-#config exists, but couldnt get working directory for some reason
-if [[ -z ${TF2WORKINGDIR+x} ]]; then
+copy_files(){
+	#for each bsp file, decompile and dump in local mapfiles dir, then move to tf2 custom dir. skip altogether if exists in custom dir.
+	for filepath in $@; do
+		filename=$(basename "$filepath")
+		strippedname="${filename%.*}"
+		if [ ! -d "$TF2CUSTOMDIR/$strippedname" ]; then
+			echo "Extracting $filename..."
+			./vpkeditcli -e / "$filepath" -o "./mapfiles"
+			echo "Copying map files to TF2 Custom directory..."
+			cp -r ./mapfiles/"$strippedname" "$TF2CUSTOMDIR"
+			echo -e "${GREEN}$strippedname done. ${NC}\n"
+		else
+			echo -e "${filename%.*} already present in TF2 custom directory. Skipping.\n"
+		fi
+	done
 
-	TF2WORKINGDIR=$(get_directory)
-fi
-
-#working dir set but is wrong
-while [ ! -f "$TF2WORKINGDIR/tf_linux64" ]
-do
-
-	echo -E "$TF2WORKINGDIR"
-	TF2WORKINGDIR=$(get_directory)
-done
-
-if [ ! -d ./mapfiles ]; then
-	mkdir ./mapfiles
-fi
-
-echo "TF2WORKINGDIR=\"$TF2WORKINGDIR\"" > tf2mapfixconfig
-
-TF2MAPSDIR="$TF2WORKINGDIR/tf/download/maps"
-TF2CUSTOMDIR="$TF2WORKINGDIR/tf/custom"
-
-#for each bsp file, decompile and dump in local mapfiles dir, then move to tf2 custom dir. skip altogether if exists in custom dir.
-for filepath in "$TF2MAPSDIR"/*; do
-	filename=$(basename "$filepath")
-	strippedname="${filename%.*}"
-	if [ ! -d "$TF2CUSTOMDIR/$strippedname" ]; then
-		echo "Extracting $filename..."
-		./vpkeditcli -e / "$filepath" -o "./mapfiles"
-		echo "Copying map files to TF2 Custom directory..."
-		cp -r ./mapfiles/"$strippedname" "$TF2CUSTOMDIR"
-		echo -e "${GREEN}$strippedname done. ${NC}\n"
-	else
-		echo -e "${filename%.*} already present in TF2 custom directory. Skipping.\n"
+	#Cleanup
+	if [ ! -z "$( ls -A './mapfiles' )" ]; then
+		echo "Cleaning up Temporary Map files"
+		rm -r ./mapfiles/*
 	fi
-done
+}
 
-#Cleanup
-if [ ! -z "$( ls -A './mapfiles' )" ]; then
-	echo "Cleaning up Temporary Map files"
-	rm -r ./mapfiles/*
+if [ $# -ne 0 ]; then
+	case "$1" in
+		-h|--help)
+			show_help
+			;;
+
+		-f|--files)
+			get_directory
+			copy_files $(select_files)
+			exit 0
+			;;
+		*)
+	esac
+	show_help
+else
+	get_directory
+	copy_files $(ls -a "$TF2MAPSDIR")
+	exit 0
 fi
