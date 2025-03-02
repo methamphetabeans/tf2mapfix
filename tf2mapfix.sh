05f1e1eac8d7457f2e200a630969b846e654df36
methamphetabeans <iamzac67@gmail.com>
2025-03-02 05:59:47 -0600

    fixed main issue. added filetype filtering. made some text green

--- tf2mapfix.sh
+++ tf2mapfix.sh
@@ -38,8 +38,8 @@ get_directory(){
 		# conf exists, is tf2 actually here?
 		if [ -f "$TF2WORKINGDIR/tf_linux64" ]; then
 			TF2WORKINGDIR=$(echo "$TF2WORKINGDIR" | sed 's/\\ / /g')
-			TF2MAPSDIR="$TF2WORKINGDIR/tf/download/maps"
-			TF2CUSTOMDIR="$TF2WORKINGDIR/tf/custom"
+			TF2MAPSDIR="$TF2WORKINGDIR"tf/download/maps
+			TF2CUSTOMDIR="$TF2WORKINGDIR"tf/custom
 
 		# tf2 is not where the config says it is, get directory from scratch
 		else
@@ -58,16 +58,16 @@ directory_fallback(){
 	if [ -f "$TF2DIR/tf_linux64" ]; then
 		# if yes, continue
 		TF2WORKINGDIR=$(echo "$TF2DIR" | sed 's/\\ / /g')
-		TF2MAPSDIR="$TF2WORKINGDIR/tf/download/maps"
-		TF2CUSTOMDIR="$TF2WORKINGDIR/tf/custom"
+		TF2MAPSDIR="$TF2WORKINGDIR"tf/download/maps
+		TF2CUSTOMDIR="$TF2WORKINGDIR"tf/custom
 	else
 		# if not, prompt for input until correct
 		while [ ! -f "$TF2WORKINGDIR/tf_linux64" ]
 		do
 			read -rep $'TF2 Directory not found. Please input your TF2 Directory location \n(This will usually end with \'.../steamapps/common/Team Fortress 2\' use the Tab key to autocomplete and avoid issues with spaces) \n' TF2ALTDIR
 			TF2WORKINGDIR=$(echo "$TF2ALTDIR" | sed 's/\\ / /g')
-			TF2MAPSDIR="$TF2WORKINGDIR/tf/download/maps"
-			TF2CUSTOMDIR="$TF2WORKINGDIR/tf/custom"
+			TF2MAPSDIR="$TF2WORKINGDIR"tf/download/maps
+			TF2CUSTOMDIR="$TF2WORKINGDIR"tf/custom
 		done
 	fi
 }
@@ -87,15 +87,17 @@ copy_files(){
 	#for each bsp file, decompile and dump in local mapfiles dir, then move to tf2 custom dir. skip altogether if exists in custom dir.
 	for filepath in $@; do
 		filename=$(basename "$filepath")
-		strippedname="${filename%.*}"
-		if [ ! -d "$TF2CUSTOMDIR/$strippedname" ]; then
-			echo "Extracting $filename..."
-			./vpkeditcli -e / "$TF2MAPSDIR/$filepath" -o "./mapfiles"
-			echo "Copying map files to TF2 Custom directory..."
-			cp -r ./mapfiles/"$strippedname" "$TF2CUSTOMDIR"
-			echo -e "${GREEN}$strippedname done. ${NC}\n"
-		else
-			echo -e "${filename%.*} already present in TF2 custom directory. Skipping.\n"
+			if [[ "$filename" == *.bsp ]]; then
+			strippedname="${filename%.*}"
+			if [ ! -d "$TF2CUSTOMDIR/$strippedname" ]; then
+				echo "Extracting $filename..."
+				./vpkeditcli -e / "$TF2MAPSDIR/$filename" -o "./mapfiles"
+				echo "Copying map files to TF2 Custom directory..."
+				cp -r ./mapfiles/"$strippedname" "$TF2CUSTOMDIR"
+				echo -e "${GREEN}$strippedname done. ${NC}\n"
+			else
+				echo -e "${filename%.*} already present in TF2 custom directory. Skipping.\n"
+			fi
 		fi
 	done
 
@@ -103,6 +105,7 @@ copy_files(){
 	if [ ! -z "$( ls -A './mapfiles' )" ]; then
 		echo "Cleaning up Temporary Map files"
 		rm -r ./mapfiles/*
+		echo -e "${GREEN}Completed${NC}"
 	fi
 }
 
@@ -122,6 +125,6 @@ if [ $# -ne 0 ]; then
 	show_help
 else
 	get_directory
-	copy_files $(ls -a "$TF2MAPSDIR")
+	copy_files $(find "$TF2MAPSDIR" -type f)
 	exit 0
 fi
